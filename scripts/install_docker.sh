4be474cd87885cb1f5321a0094899db8d66d1ac5
Michael O'Connor <oconnor.michael.stephen@gmail.com>
2016-11-21 13:24:08 -0500

    Fixed install script

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -11,8 +11,6 @@ _default_tcp_enabled=false
 _default_tcp_address="tcp://0.0.0.0:2375"
 
 dockerServiceDir=/etc/systemd/system/docker.service.d
-mkdir $dockerServiceDir
-touch $dockerServiceDir/docker.conf
 
 timeStamp=$(date "+%Y-%m-%d-%H%M")
 logFile=/var/log/cons3rt-install-docker-${timeStamp}.log
@@ -28,25 +26,25 @@ function configure_docker() {
   systemctl enable docker
 
   if [ $(getProperty $TCP_ENABLED_PROP) ]; then
-    docker_tcp_enabled=$_default_tcp_enabled
-    logMessage "Docker will not be configured to listen over tcp"
-  else
     docker_tcp_enabled=$(getProperty $TCP_ENABLED_PROP)
     logMessage "Docker will be configured to listen over tcp"
+  else
+    docker_tcp_enabled=$_default_tcp_enabled
+    logMessage "Docker will not be configured to listen over tcp"
   fi
   if [ $(getProperty $TCP_ADDRESS_PROP) ]; then
-    docker_tcp_address=$_default_tcp_address
-  else
     docker_tcp_address=$(getProperty $TCP_ADDRESS_PROP)
+  else
+    docker_tcp_address=$_default_tcp_address
   fi
   mkdir $dockerServiceDir
   touch $dockerServiceDir/docker.conf
 
   if [ "$docker_tcp_enabled" = "true" ]; then
     logMessage "Docker will be configured to listen over tcp on this address: $docker_tcp_address"
-    echo -e "[Service]\nExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock -H $docker_tcp_address"
+    echo -e "[Service]\nExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock -H $docker_tcp_address" >> $dockerServiceDir/docker.conf
   else
-    echo -e "[Service]\nExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock"
+    echo -e "[Service]\nExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock" >> $dockerServiceDir/docker.conf
   fi
 
   logMessage "starting docker daemon"
